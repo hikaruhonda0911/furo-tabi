177ab13771ec6803725e16ddfb2eab4cca7cdf1d
本田光 <hikaruhonda@MacBook-Air.local>
2026-02-27 09:06:03 +0900

    Update  tailwind.comfig.js

---  tailwind.comfig.js
+++  tailwind.comfig.js
@@ -4,28 +4,28 @@ module.exports = {
       colors: {
         furotabi: {
           // 背景
-          base: "#FBFDFE",
+          base: '#FBFDFE',
 
           // 文字・主要UI高級感のある紺。
-          main: "#020617",
+          main: '#020617',
 
           // アクセント：澄んだ水。
-          accent: "#7DD3FC",
+          accent: '#7DD3FC',
 
           // 補助テキスト：ノイズを削ぎ落とした薄いグレー。
-          muted: "#64748B",
+          muted: '#64748B',
 
           // 境界線
-          border: "#E2E8F0",
+          border: '#E2E8F0',
 
           //アクセントの深色
-          "accent-dark": "#38BDF8",
+          'accent-dark': '#38BDF8',
         },
       },
       // デザイン哲学「引き算」を強調するための極細フォント設定
       fontWeight: {
-        "extra-light": "100",
-        light: "200",
+        'extra-light': '100',
+        light: '200',
       },
     },
   },
